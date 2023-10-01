7eb8ab0a20f729a61147252d38d84adde481715a
whiplade <nmawira13@gmail.com>
2023-10-01 16:19:33 +0300

    models.py

--- server/models.py
+++ server/models.py
@@ -9,7 +9,7 @@ class Restaurant(db.Model, SerializerMixin):
     serializer_rules = ('-restaurant.pizzas',)
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, unique=True, nullable=False)
+    name = db.Column(db.String, nullable=False)
     address = db.Column(db.String, unique=True, nullable=False)
 
     pizzas = db.relationship('Pizza', secondary='restaurantpizza', backref='restaurant')
@@ -29,25 +29,37 @@ class Pizza(db.Model, SerializerMixin):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
     ingredients = db.Column(db.String, nullable=False)
-    price = db.Column(db.Integer, nullable=False)
 
     restaurants = db.relationship('Restaurant', secondary='restaurantpizza', backref='pizza')
 
-    @validates('price')
-    def validate_price(self, key, price):
-        if not 1 <= price <= 30:
-            raise ValueError("Price must be between $1 and $30.")
-        return price
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "ingredients": self.ingredients,
+        }
 
-   
+ 
 class RestaurantPizza(db.Model, SerializerMixin):
     __tablename__ = 'restaurantpizza'
     serializer_rules = ('-restaurant.pizzas', '-pizza.restaurants')
 
     id = db.Column(db.Integer, primary_key=True)
+    price = db.Column(db.Integer, nullable=False)
+
     restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurants.id'))
     pizza_id = db.Column(db.Integer, db.ForeignKey('pizzas.id'))
     
-
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "price": self.price,
+            "restaurant_id": self.restaurant_id,
+            "pizza_id": self.pizza_id,
+        }
     
-    
+    @validates('price')
+    def validate_price(self, key, price):
+        if not 1 <= price <= 30:
+            raise ValueError("Price must be between $1 and $30.")
+        return price
