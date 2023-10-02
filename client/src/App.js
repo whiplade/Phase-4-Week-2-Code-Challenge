4081b7b4de24853832b5426711c4fe835952d7ef
whiplade <nmawira13@gmail.com>
2023-10-02 20:30:09 +0300

    App.js

---
+++ client/src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import RestaurantList from './RestaurantList';
+import RestaurantDetails from './RestaurantDetails';
+import CreateRestaurantPizza from './CreateRestPizza';
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <h1>Welcome to the Pizza Restaurant App</h1>
+        <nav>
+          <ul>
+            <li>
+              <a href="/">Home</a>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<RestaurantList/>} />
+          <Route path="/restaurants/:restaurantId" element={<RestaurantDetails/>} />
+          <Route path="/create-restaurant-pizza" element={<CreateRestaurantPizza/>} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
